2de069e2a3cf39e5ab4067e5509060fa04419d09
aravintharajp <50869424+aravintharajp@users.noreply.github.com>
2019-05-25 16:05:56 +0530

    Update and rename README.md to b.c

---
+++ b.c
@@ -0,0 +1,16 @@
+int main ()
+{
+    char str[50], temp;
+    int i, j;
+    cout << "Enter a string : ";
+    gets(str);
+    j = strlen(str) - 1;
+    for (i = 0; i < j; i++,j--)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    cout << "\nReverse string : " << str;
+    return 0;
+}
